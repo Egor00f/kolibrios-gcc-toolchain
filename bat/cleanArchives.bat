d69c66df539e306e2a4aedf247c138f11c9cc9b8
Egor00f <y.yarin@inbox.ru>
2026-03-19 21:15:18 +0500

    delete bat && fix CMakeLists.txt

--- bat/cleanArchives.bat
+++
@@ -1,4 +0,0 @@
-set TOOLCHAIN_DIR=%1
-
-del /Q %TOOLCHAIN_DIR%\kos32-toolchain.7z
-del /Q %TOOLCHAIN_DIR%\win32\mingw32\sdk-28-10-16.7z
--- bat/updateLibraries.bat
+++
@@ -1,5 +0,0 @@
-set TOOLCHAIN_DIR=%1
-
-wget -r --no-parent http://builds.kolibrios.org/en_US/data/contrib/sdk/lib/
-move /y builds.kolibrios.org\en_US\data\contrib\sdk\lib\* %TOOLCHAIN_DIR%\win32\mingw32\lib
-rd /s /q builds.kolibrios.org
